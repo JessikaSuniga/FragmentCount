14c695a478a3c0a61b924541f7ba7ee32c743a77
Jessika Suniga <jessikasuniga@gmail.com>
2021-11-16 12:23:23 -0300

    FragmentCount3

---
+++ app/src/main/java/com/example/fragmentappcounter/Fragment1Count.java
@@ -0,0 +1,10 @@
+package com.example.fragmentappcounter;
+
+public class Fragment1Count extends FragmentCount {
+
+    @Override
+    protected int getFragmentView() {
+        return R.layout.fragment_count1;
+    }
+
+}
---
+++ app/src/main/java/com/example/fragmentappcounter/Fragment2Count.java
@@ -0,0 +1,10 @@
+package com.example.fragmentappcounter;
+
+public class Fragment2Count extends FragmentCount {
+
+    @Override
+    protected int getFragmentView() {
+        return R.layout.fragment_count2;
+    }
+
+}
